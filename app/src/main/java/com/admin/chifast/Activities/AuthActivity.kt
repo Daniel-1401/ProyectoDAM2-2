89b044259eb3ef7fa9add9785d82df37b9dcb5f6
Daniel <carlosdaniel-14@hotmail.com>
2021-11-30 11:19:37 -0500

    persistencia del login - CARLOS

--- app/src/main/java/com/admin/chifast/Activities/AuthActivity.kt
+++ app/src/main/java/com/admin/chifast/Activities/AuthActivity.kt
@@ -1,8 +1,10 @@
 package com.admin.chifast.Activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.admin.chifast.R
 import com.google.firebase.auth.FirebaseAuth
@@ -12,8 +14,25 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-        
+        session()
         setup()
+    }
+    override fun onStart(){
+        super.onStart()
+
+        conenidoAuth.visibility = View.VISIBLE
+
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+
+        if (email != null){
+            conenidoAuth.visibility = View.INVISIBLE
+            showHome(email)
+        }
+
     }
 
     private fun setup() {
